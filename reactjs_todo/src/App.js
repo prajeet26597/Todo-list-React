b74f1d4a5f064cb055a63dcc8de3c519a3a191a4
Prajeet Rajaganesan <prajeetrajaganesan@gmail.com>
2021-03-31 15:18:10 +0200

    First commit to github

---
+++ reactjs_todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo_by_praj">
+      
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
