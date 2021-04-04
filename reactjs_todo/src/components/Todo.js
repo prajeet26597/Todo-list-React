327fa1fa3435395007d208c51b6054cd96d9b389
Prajeet Rajaganesan <prajeetrajaganesan@gmail.com>
2021-04-04 20:15:49 +0200

    Update Todo.js 04_04_2021

---
+++ reactjs_todo/src/components/Todo.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+import TodoList from './todoList'
+
+function Todo() {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    })
+
+
+    return todos.map((todo, index) => (
+        <div className = {todo.isComplete ? 'todo-row complete' : 
+        'todo-row'} key={index}>
+
+            </div>
+    ))
+}
+
+export default Todo
