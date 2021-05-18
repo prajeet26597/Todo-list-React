714e88c4a66456bd8352227cea60eb5590ffd56b
Prajeet Rajaganesan <prajeetrajaganesan@gmail.com>
2021-05-18 18:03:22 +0200

    Next Step CSS

--- reactjs_todo/src/components/Todo.js
+++ reactjs_todo/src/components/Todo.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
 import TodoForm from './TodoForm'
-import TodoList from './todoList'
+import TodoList from './TodoList'
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { TiEdit } from 'react-icons/ti'
 
-function Todo() {
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
-    })
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value);
+        setEdit({
+            id: null,
+            value: ''
+        });
+    };
+
+    if(edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    }
 
 
     return todos.map((todo, index) => (
@@ -14,13 +28,20 @@ function Todo() {
         'todo-row'} key={index}>
 
                 <div key = {todo.id} onClick = {() => completeTodo(todo.id)}>
+                    {todo.text}
                 </div>
                 <div className="icons">
-                    
+                    <RiCloseCircleLine 
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'
+                    />
+                    <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className='edit-icon'
+                    />
                 </div>
 
             </div>
-    ))
+    ));
 }
 
 export default Todo
--- reactjs_todo/src/components/TodoList.js
+++ reactjs_todo/src/components/TodoList.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
+
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -12,12 +14,37 @@ function TodoList() {
         const newTodos = [todo, ...todos];
 
         setTodos(newTodos);
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo
+        });
+        setTodos(updatedTodos);
     }
 
     return (
         <div>
             <h1>Plan for today</h1>
             <TodoForm onSubmit = {addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 }
